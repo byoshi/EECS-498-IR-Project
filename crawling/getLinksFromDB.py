40981fcdd81f759740f5e8934861ede2d1993617
Yiying Li <yyli@umich.edu>
2014-04-05 08:54:07 +0400

    added crawling

--- crawling/getLinksFromDB.py
+++ crawling/getLinksFromDB.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python
 import mysql.connector
 import time
+from Queue import Queue
 
 class WikiDB:
     def __init__(self):
@@ -41,8 +43,11 @@ class WikiDB:
                 ret = r
 
             idx += 1
-
-        if ret[1] == 1:
+        
+        if ret is None:
+            pass
+            # print "no result: ", title
+        elif ret[1] == 1:
             # print "redirect:", title
             ret = self.get_page_id_if_redirect(ret[0])
         else:
@@ -51,7 +56,6 @@ class WikiDB:
         return ret
 
     def get_page_links(self, id):
-        start = time.time()
         self.cursor.execute("select pl_namespace, pl_title from pagelinks where pl_from=%s", (id, ))
         titles = []
         for r in self.cursor:
@@ -60,19 +64,33 @@ class WikiDB:
 
         ret = []
         for r in titles:
-            page_id, page_title = self.get_page_id_from_title(r)
-            ret.append((page_id, page_title))
-            # print page_id, page_title
-            end = time.time()
-            print "Current:", end - start
+            page_id_title = self.get_page_id_from_title(r)
+            if page_id_title is not None:
+                ret.append(page_id_title)
+                # print page_id, page_title
 
-        end = time.time()
-        print "Total:", end - start
         return ret
 
-if __name__ == '__main__':
+
+def crawl():
     db = WikiDB()
     db.connect()
 
-    page_id, page_title = db.get_page_id_from_title("World_War_II")
-    links = db.get_page_links(page_id)
+    visited = set()
+    q = Queue()
+    page_id_title = db.get_page_id_from_title("World_War_II")
+    q.put(page_id_title[0])
+    stop_len = 100
+    g_start = time.time()
+    while not q.empty() and len(visited) < stop_len:
+        start = time.time()
+        page_id = q.get()
+        links = db.get_page_links(page_id)
+        visited.add(page_id)
+        for l in links:
+            q.put(l[0])
+        end = time.time()
+        print len(visited), "last link:", end - start, "time left", (end - g_start)/len(visited) * stop_len
+
+if __name__ == '__main__':
+    crawl()
