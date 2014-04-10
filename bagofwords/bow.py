1526f05688bc6e885a70c30b5d4e715d5664cc00
byoshi <byoshi@umich.edu>
2014-04-09 23:25:35 -0400

    BOW working.

--- bagofwords/bow.py
+++ bagofwords/bow.py
@@ -2,25 +2,55 @@ import collections, re, sys, os
 from parse import tokenizeIndividualDocument
 
 
-def bagOfWords():
+def bagOfWords(file_to_tf_dict, k_means_filename):
+	all_words = []
+
+	#Build a list of all tokens
+	for tf_dict in file_to_tf_dict.values():
+		for token in tf_dict.keys():
+			if token not in all_words:
+				all_words.append(token)
+
+	k_means_file = open(k_means_filename, 'w')
+	file_bow = ""
+	# words_str = ""
+	# for word in all_words:
+	# 	words_str += word
+	# 	words_str += " "
+
+	# words_str += "\n"
+	# k_means_file.write(words_str)
+
+	#Create and print bow vectors
+	for tf_dict in file_to_tf_dict.values():
+		for word in all_words:
+			if word in tf_dict.keys():
+				file_bow += str(tf_dict[word])
+			else:
+				file_bow += "0"
+			file_bow += " "
+
+		k_means_file.write(file_bow[0:len(file_bow)-1] + "\n")
+		file_bow = ""
+	k_means_file.close()
  
 def main():
-	if len(sys.argv) != 3:
-		print "usage: ./bow.py [documents directory path] [stopwords file]"
+	if len(sys.argv) != 4:
+		print "usage: ./bow.py [documents directory path] [stopwords file] [k-means output filename]"
+		sys.exit(1)
 
 	directory = sys.argv[1]
 	stopwords_file = sys.argv[2]
+	k_means_filename = sys.argv[3]
 
 	file_to_tf_dict = {}
 	for dirpath, dirnames, filenames in os.walk(directory):
 		for f in filenames:
 			cur_file = open(directory + "/" + f, 'r')
 			file_to_tf_dict[f] = tokenizeIndividualDocument(cur_file.read(), stopwords_file)
+			cur_file.close()
 
-	for filename, tf_dict in file_to_tf_dict.iteritems():
-		print "Current file:", filename
-		for token, tf in tf_dict.iteritems():
-			print token, tf
+	bagOfWords(file_to_tf_dict, k_means_filename)
 
 if __name__ == '__main__':
 	main()
